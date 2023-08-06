6bf0ea46be2eb5352b233995d1039434c186cefa
Tatyana Golikova <las-tochka2200@mail.ru>
2023-08-07 00:53:06 +0200

    подготовка бэкенда

---
+++ controllers/users.js
@@ -0,0 +1,73 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+};
+
+module.exports.getUserById = (req, res) => {
+  if (req.params.userId.length === 24) {
+    User.findById(req.params.userId)
+      .then((user) => {
+        if (!user) {
+          res.status(404).send({ message: 'Пользователь не найден.' });
+          return;
+        }
+        res.send(user);
+      })
+      .catch(() => res.status(404).send({ message: 'Пользователь не найден.' }));
+  } else {
+    res.status(400).send({ message: 'Неверный _id' });
+  }
+};
+
+module.exports.addUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    // вернём записанные в базу данные
+    .then((user) => res.status(201).send(user))
+    // данные не записались, вернём ошибку
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
+module.exports.editUserData = (req, res) => {
+  const { name, about } = req.body;
+  if (req.user._id) {
+    User.findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: true })
+      .then((user) => res.send(user))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(404).send({ message: 'Пользователь не найден' });
+        }
+      });
+  } else {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
+};
+
+module.exports.editUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  if (req.user._id) {
+    User.findByIdAndUpdate(req.user._id, { avatar }, { new: 'true', runValidators: true })
+      .then((user) => res.send(user))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(404).send({ message: 'Пользователь не найден' });
+        }
+      });
+  } else {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
+};
